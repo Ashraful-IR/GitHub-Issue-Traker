9284f73db0c0d35177df3ce3e1e63a37aa53fb7f
Ashraful-IR <rifo0118@gmail.com>
2026-03-08 16:50:43 +0600

    Modal Full Done

--- Script/home.js
+++ Script/home.js
@@ -7,6 +7,27 @@ const allBtn = document.getElementById("all-btn");
 const closedBtn = document.getElementById("closed-btn");
 const ClosedIssues = document.getElementById("Closed-Issues");
 
+const loadAllIssues = () => {
+  displaySpinner(true);
+  const url = `https://phi-lab-server.vercel.app/api/v1/lab/issues`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      displayAllIssues(data.data);
+      displayOpenIssues(data.data);
+      displayClosedIssues(data.data);
+    });
+};
+
+const showIssuesDetails = (id) => {
+  const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      displayModalDetails(data.data);
+    });
+};
+
 const displaySpinner = (status) => {
   const spinner = document.getElementById("spinner");
   const alIssues = document.getElementById("All-Issues");
@@ -18,7 +39,6 @@ const displaySpinner = (status) => {
     openIssues.classList.add("hidden");
     closedIssues.classList.add("hidden");
   } else {
-    
     alIssues.classList.remove("hidden");
     spinner.classList.add("hidden");
     openIssues.classList.add("hidden");
@@ -26,16 +46,59 @@ const displaySpinner = (status) => {
   }
 };
 
-const loadAllIssues = () => {
-  displaySpinner(true);
-  const url = `https://phi-lab-server.vercel.app/api/v1/lab/issues`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      displayAllIssues(data.data);
-      displayOpenIssues(data.data);
-      displayClosedIssues(data.data);
-    });
+displayModalDetails = (issues) => {
+  document.getElementById("my_modal_5").showModal();
+  const modalDetailsContainer = document.getElementById("details-container");
+  modalDetailsContainer.innerHTML =` 
+
+        <div>
+          <p class="text-[24px] font-bold text-[#1f2937]">
+            ${issues.title}
+          </p>
+        </div>
+
+        <div class="flex justify-start items-left gap-5">
+          <span class="text-[16px] text-[#fa0f36] font-medium bg-red-100 w-20 text-center rounded-full">${issues.status}</span>
+          <p class="text-[12px] text-[#64748b]">Opened by ${issues.assignee}</p>
+          <p class="text-[12px] text-[#64748b]">${new Date(issues.createdAt).toLocaleDateString()}</p>
+        </div>
+
+        <div class="flex justify-start items-left gap-5">
+          <div class="border border-[#fecaca] gap-2 w-40 bg-[#feecec] rounded-[100px] p-2">
+              <p class="text-center text-[12px] font-bold text-[#ef4444]"><i class="fa-solid fa-bug"></i>${issues.labels[0]}</p>
+            </div>
+            ${
+              issues.labels[1]
+                ? `<div class="border border-[#fde68a] gap-2 w-40 bg-[#fff8db] rounded-[100px] p-2">
+                    <p class="text-center text-[12px] font-bold text-[#d97706]"><i class="fa-solid fa-hands-helping"></i> ${issues.labels[1]}</p>
+                  </div>`
+                : ""
+            }
+        </div>
+
+        <div>
+          <p class="text-[16px] text-[#64748b]">
+            ${issues.description}
+          </p>
+        </div>
+
+        <div class="flex justify-between items-center gap-5 bg-[#f8fafc] rounded-md w-full p-5">
+              <div class="flex flex-col justify-between items-start gap-4">
+                <p class="text-[16px] text-[#64748b]">Assignee</p>
+                <p class="text-[16px] font-semibold text-[#030303]">${issues.assignee}</p>
+              </div>
+
+              <div class="flex flex-col justify-between items-start gap-4">
+                <p class="text-[16px] text-[#64748b]">Priority</p>
+                <span class="text-[16px] text-[#fa0f36] font-medium bg-red-100 w-20 text-center rounded-full">${issues.priority}</span>
+              </div>
+          
+        </div>
+
+  `;
+
+  
+  
 };
 
 displayAllIssues = (issues) => {
@@ -49,7 +112,7 @@ displayAllIssues = (issues) => {
     const issueElement = document.createElement("div");
     issueElement.classList.add("issue");
     issueElement.innerHTML = `
-            <div onclick="my_modal_5.showModal()" class="bg-white w-full rounded-lg p-5 gap-3 flex flex-col ${borderColor}">
+            <div onclick="showIssuesDetails(${issue.id})" class="bg-white w-full rounded-lg p-5 gap-3 flex flex-col ${borderColor}">
             <div class="flex justify-between items-center">
             ${issue.status === "open" ? '<img src="../assets/Open-Status.png" alt="">' : ""}
             ${issue.status === "open" ? `<span class="text-[16px] text-[#fa0f36] font-medium bg-red-100 w-20 text-center rounded-full">${issue.priority}</span>` : ""}
@@ -98,7 +161,7 @@ displayOpenIssues = (issues) => {
       const issueElement = document.createElement("div");
       issueElement.classList.add("issue");
       issueElement.innerHTML = `
-        <div onclick="my_modal_5.showModal()" class="bg-white w-full rounded-lg p-5 gap-3 flex flex-col border-t-4 border-green-500">
+        <div onclick="showIssuesDetails(${issue.id})" class="bg-white w-full rounded-lg p-5 gap-3 flex flex-col border-t-4 border-green-500">
           <div class="flex justify-between items-center">
             <img src="../assets/Open-Status.png" alt="">
             <span class="text-[16px] text-[#fa0f36] font-medium bg-red-100 w-20 text-center rounded-full">${issue.priority}</span>
@@ -144,7 +207,7 @@ displayClosedIssues = (issues) => {
       const issueElement = document.createElement("div");
       issueElement.classList.add("issue");
       issueElement.innerHTML = `
-        <div onclick="my_modal_5.showModal()" class="bg-white w-full rounded-lg p-5 gap-3 flex flex-col border-t-4 border-purple-500">
+        <div onclick="showIssuesDetails(${issue.id})" class="bg-white w-full rounded-lg p-5 gap-3 flex flex-col border-t-4 border-purple-500">
           <div class="flex justify-between items-center">
             <img src="../assets/Status.png" alt="">
             <span class="text-[16px] text-[#fa0f36] font-medium bg-red-100 w-20 text-center rounded-full">${issue.priority}</span>
@@ -188,13 +251,16 @@ const updateIssueCount = () => {
   let visibleIssueCount = [];
   if (!allIssuesContainer.classList.contains("hidden")) {
     visibleIssueCount = [...allIssuesContainer.children].filter(
-      issue => issue.offsetParent !== null);
+      (issue) => issue.offsetParent !== null,
+    );
   } else if (!openIssuesContainer.classList.contains("hidden")) {
     visibleIssueCount = [...openIssuesContainer.children].filter(
-      issue => issue.offsetParent !== null);
+      (issue) => issue.offsetParent !== null,
+    );
   } else if (!closedIssuesContainer.classList.contains("hidden")) {
     visibleIssueCount = [...closedIssuesContainer.children].filter(
-      issue => issue.offsetParent !== null);
+      (issue) => issue.offsetParent !== null,
+    );
   }
   allIssueCount.innerText = visibleIssueCount.length + " Issues";
 };
